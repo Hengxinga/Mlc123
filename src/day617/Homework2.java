1986d2fae6b18510c1a1d8d461ecde001c71367e
Hengxinga <13027057000@163.com>
2020-06-17 21:36:16 +0800

    孟令冲作业6.17

---
+++ src/day617/Homework2.java
@@ -0,0 +1,17 @@
+package day617;
+
+public class Homework2 {
+
+    public static void main(String[] args) {
+
+        for(int i = 1; i<=10 ; i++) {
+            for (int j = 1; j <= 10 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i * 2 - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
